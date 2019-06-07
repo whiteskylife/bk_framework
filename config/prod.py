3df53156e471357b99993dfeb207855e650609bf
Administrator <981577040@qq.com>
2019-06-07 19:19:40 +0800

    hello

--- config/prod.py
+++ config/prod.py
@@ -18,12 +18,12 @@ RUN_MODE = 'PRODUCT'
 DATABASES.update(
     {
         'default': {
-            'ENGINE': '',
-            'NAME': '',  # 数据库名
-            'USER': '',  # 数据库用户
-            'PASSWORD': '',  # 数据库密码
-            'HOST': '',  # 数据库主机
-            'PORT': '',  # 数据库端口
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': APP_CODE,
+            'USER': 'root',
+            'PASSWORD': '123456',
+            'HOST': '192.168.1.119',
+            'PORT': '3306',
         },
     }
 )
